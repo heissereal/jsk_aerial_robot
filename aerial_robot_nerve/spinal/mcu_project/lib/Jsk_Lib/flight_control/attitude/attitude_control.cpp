fd16dd97ed055c7b83f6b8ef3a5aecdad12f08fc
heissereal <miyamichi@jsk.imi.i.u-tokyo.ac.jp>
2023-11-30 17:22:33 +0900

    add SIMULATION to make build success

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/flight_control/attitude/attitude_control.cpp
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/flight_control/attitude/attitude_control.cpp
@@ -669,16 +669,24 @@ void AttitudeController::pwmTestCallback(const std_msgs::Float32& pwm_msg)
 
 void AttitudeController::pwmIndivTestCallback(const spinal::PwmState& pwm_msg)
 {
-  if (pwm_msg.index_length == pwm_msg.percentage_length)
-    {
-      for(int i = 0; i < pwm_msg.index_length; i++)
-        {
-          size_t index = pwm_msg.index[i];
-          target_pwm_[index] = pwm_msg.percentage[i];
-          // target_pwm_[pwm_msg.index[i]] = pwm_msg.percentage[i];
-        }
-      return;
-    }
+#ifdef SIMULATION
+  if (pwm_msg.index.size() == pwm_msg.percentage.size())
+#else
+    if (pwm_msg.index_length == pwm_msg.percentage_length)
+#endif
+      {
+#ifdef SIMULATION
+        for(int i = 0; i < pwm_msg.index.size(); i++)
+#else
+          for(int i = 0; i < pwm_msg.index_length; i++)
+#endif
+            {
+              size_t index = pwm_msg.index[i];
+              target_pwm_[index] = pwm_msg.percentage[i];
+              // target_pwm_[pwm_msg.index[i]] = pwm_msg.percentage[i];
+            }
+        return;
+      }
 }
 
 
